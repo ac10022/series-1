de43cc7e19c6a7b5584cecde0068e8f218a12280
ac10022 <119063466+ac10022@users.noreply.github.com>
2024-09-25 15:06:50 +0100

    problem 1

---
+++ problem 1/Permutations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace wrytangle
+{
+    internal class Permutations<T>
+    {
+        public IEnumerable<T[]> GetPermutations(T[] values, int fromInd = 0)
+        {
+            if (fromInd + 1 == values.Length)
+                yield return values;
+            else
+            {
+                foreach (var v in GetPermutations(values, fromInd + 1))
+                    yield return v;
+
+                for (var i = fromInd + 1; i < values.Length; i++)
+                {
+                    SwapValues(values, fromInd, i);
+                    foreach (var v in GetPermutations(values, fromInd + 1))
+                        yield return v;
+                    SwapValues(values, fromInd, i);
+                }
+            }
+        }
+
+        private void SwapValues(T[] values, int pos1, int pos2)
+        {
+            if (pos1 != pos2)
+            {
+                T tmp = values[pos1];
+                values[pos1] = values[pos2];
+                values[pos2] = tmp;
+            }
+        }
+    }
+
+    internal class PermutationsHelper
+    {
+        // assume 2|len
+        public bool IntComparisonPredicate(int[] values)
+        {
+            for (int i = 0; i < (values.Length / 2) - 1; i++)
+            {
+                if (values[2 * i] > values[2 * (i + 1)]) return false;
+            }
+            return true;
+        }
+
+        public int FlattenAndSum(int[] values)
+        {
+            int total = 0;
+            for (int i = 0; i < (values.Length / 2); i++)
+            {
+                total += int.Parse($"{values[2 * i]}{values[2 * i + 1]}");
+            }
+            return total;
+        }
+    }
+}
---
+++ problem 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrytangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutations<int> p = new Permutations<int>();
+            PermutationsHelper ph = new PermutationsHelper();
+
+            List<int[]> res = p.GetPermutations(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+                               .Select(v => (int[])v.Clone())
+                               // predicate
+                               .Where(v => ph.IntComparisonPredicate(v))
+                               .ToList();
+
+            int combinedTotal = 0;
+
+            foreach (var item in res)
+            {
+                Console.WriteLine(string.Join(", ", item));
+                combinedTotal += ph.FlattenAndSum(item);
+            }
+
+            Console.WriteLine($"Combined total: {combinedTotal}");
+        }
+    }
+}
