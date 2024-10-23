4408e787b7c4c39662b1ae78078a91818f2505b1
ac10022 <119063466+ac10022@users.noreply.github.com>
2024-10-23 09:31:17 +0100

    problem 8

---
+++ problem 8/Permutations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace problem8
+{
+    internal class Permutations<T>
+    {
+        public IEnumerable<T[]> GetPermutations(T[] values, int fromInd = 0)
+        {
+            if (fromInd + 1 == values.Length)
+                yield return values;
+            else
+            {
+                foreach (var v in GetPermutations(values, fromInd + 1))
+                    yield return v;
+
+                for (var i = fromInd + 1; i < values.Length; i++)
+                {
+                    SwapValues(values, fromInd, i);
+                    foreach (var v in GetPermutations(values, fromInd + 1))
+                        yield return v;
+                    SwapValues(values, fromInd, i);
+                }
+            }
+        }
+
+        private void SwapValues(T[] values, int pos1, int pos2)
+        {
+            if (pos1 != pos2)
+            {
+                T tmp = values[pos1];
+                values[pos1] = values[pos2];
+                values[pos2] = tmp;
+            }
+        }
+
+        public int VowelPredicate(string input)
+        {
+            return new Regex(@"[AEIOU]{2,}").Matches(input).Count;
+        }
+    }
+}
---
+++ problem 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace problem8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Permutations<char> p = new Permutations<char>();
+            List<char[]> res = p.GetPermutations(new char[] { 'R', 'I', 'T', 'A', 'N', 'G', 'L', 'E' })
+                               .Select(v => (char[])v.Clone())
+                               .Where(v => p.VowelPredicate(new string(v)) >= 1)
+                               .ToList();
+
+            foreach (char[] item in res)
+            {
+                Console.WriteLine(new string(item));
+            }
+            Console.WriteLine(res.Count);
+
+        }
+    }
+}
