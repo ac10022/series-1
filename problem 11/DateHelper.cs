f7cdd233a331409d200e130d92be4cc28402305b
ac10022 <119063466+ac10022@users.noreply.github.com>
2024-11-02 09:53:16 +0000

    Add files via upload

---
+++ problem 11/DateHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ritangle
+{
+    internal class DateHelper
+    {
+        public bool IsDivisorDate(DateTime dt)
+        {
+            return IsDivisor(dt.Month, dt.Day) && IsDivisor(dt.Year - 2000, dt.Month) && dt.Day > 1 && dt.Month > dt.Day && (dt.Year - 2000 > dt.Month);
+        }
+
+        public bool IsDivisor(int dividend, int divisor) => (dividend / (double)divisor) == Math.Floor(dividend / (double)divisor);
+
+        public string ToDateString(DateTime dt) => $"{dt.Day}{dt.Month.ToString().PadLeft(2, '0')}{dt.Year}";
+    }
+}
---
+++ problem 11/Program.cs
@@ -0,0 +1,49 @@
+﻿using ritangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrytangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateHelper dh = new DateHelper();
+            List<DateTime> list = new List<DateTime>();
+
+            DateTime dateTime = DateTime.Today;
+
+            while (dateTime < new DateTime(2099, 12, 31))
+            {
+                if (dh.IsDivisorDate(dateTime))
+                {
+                    Console.WriteLine(dh.ToDateString(dateTime));
+                    list.Add(dateTime);
+                }
+
+                dateTime = dateTime.AddDays(1);
+            }
+
+            List<Tuple<double, DateTime, DateTime>> timeSpans = new List<Tuple<double, DateTime, DateTime>>();
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                double span = list[i + 1].Subtract(list[i]).TotalDays;
+                timeSpans.Add(new Tuple<double, DateTime, DateTime>(span, list[i], list[i + 1]));
+            }
+
+            foreach (var item in timeSpans)
+            {
+                Console.WriteLine(item.Item1);
+            }
+
+            Tuple<double, DateTime, DateTime> max = timeSpans.OrderByDescending(x => x.Item1).FirstOrDefault();
+
+            Console.WriteLine($"Max: between {max.Item2}, {max.Item3} => {max.Item1} days");
+        }
+    }
+}
