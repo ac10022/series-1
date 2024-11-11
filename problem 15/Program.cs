78d25e8a27bcceb6430740242bdec72b0d92ec73
ac10022 <119063466+ac10022@users.noreply.github.com>
2024-11-11 15:34:55 +0000

    Add files via upload

---
+++ problem 15/Program.cs
@@ -0,0 +1,19 @@
+﻿using ritangle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wrytangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleNumbers tn = new TriangleNumbers();
+            tn.Compute();
+        }
+    }
+}
---
+++ problem 15/TriangleNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ritangle
+{
+    internal class TriangleNumbers
+    {
+        public void Compute()
+        {
+            Dictionary<int, char> triangleNumbers = new Dictionary<int, char>();
+            for (int i = 1; i < 447; i++)
+            {
+                triangleNumbers.Add((int)(0.5 * i * (i + 1)), "TRIANGLE"[(i-1)%8]);
+            }
+
+            Dictionary<int, char> nineties = new Dictionary<int, char>();
+            for (int j = 1; j < 1112; j++)
+            {
+                nineties.Add(90 * j, "RITANGLE"[(j - 1) % 8]);
+            }
+
+            List<int> pairs = triangleNumbers.Where(x => nineties.Contains(new KeyValuePair<int, char>(x.Key, x.Value))).Select(x => x.Key).ToList();
+
+            foreach (int item in pairs)
+            {
+                Console.WriteLine($"{item} => T: {triangleNumbers[item]}, N: {nineties[item]}");
+            }
+        }
+    }
+}
